c9c7f6acebe3ebef1b52257dc0cc77cc35cad861
eunkenlow <eunkenlow@gmail.com>
2016-11-11 12:36:04 +0000

    added thumbails

--- controller/getHeadline.js
+++ controller/getHeadline.js
@@ -2,18 +2,20 @@ var articles = new ArticleList();
 
 function getTitle(articles) {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?');
+  ourRequest.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/world?show-fields=thumbnail');
 
   ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
     ourData.response.results.forEach(function(object, index) {
       var newdiv = document.createElement("div");
-      newdiv.innerHTML = "<li><a href=" + "#" + index + " id=" +index+">"+ object.webTitle + "</a></li>";
+      var newimg = document.createElement("div");
+      newimg.innerHTML = "<img src=" + object.fields.thumbnail + ">";
+      newdiv.innerHTML = "<li><a href=" + "#" + index + " id=" +index+">"+ object.webTitle + "</a></li><br>";
+      document.getElementById('news').appendChild(newimg);
       document.getElementById('news').appendChild(newdiv);
       var article = new Article();
       article.addHeadline(object.webTitle);
       article.addUrl(object.webUrl);
-      console.log(articles)
       articles.addArticleToList(article);
 
     });
