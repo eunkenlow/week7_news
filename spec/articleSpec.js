2d4a8a5a1f48eb76697b2ce4f00a5eaa8435bee9
Royston <rlqhenson@hotmail.com>
2016-11-11 12:04:14 +0000

    added unit test for redirectin to full article

--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -9,6 +9,12 @@ function unitAddSummary() {
   article.addSummary('Donald Trump');
   expect.isTrue(article._summary === 'Donald Trump');
 }
+function unitAddUrl() {
+  var article = new Article();
+  article.addURL('www.donaldtrump.com');
+  expect.isTrue(article._url === 'www.donaldtrump.com');
+}
 
 unitAddTitle();
 unitAddSummary();
+unitAddUrl();
