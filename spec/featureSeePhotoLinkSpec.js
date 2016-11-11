7279af3187ee26ba5f4ec1198e9708d73c01fc36
Royston <rlqhenson@hotmail.com>
2016-11-11 12:29:18 +0000

    added feature test for redirection to full article close#7

---
+++ spec/featureSeePhotoLinkSpec.js
@@ -0,0 +1,7 @@
+function seePhotoLink() {
+  if(document.getElementById('photo0') === null) {
+    console.log("Feature test: Test failed");
+  } else {
+    console.log("Feature test: Test passed");
+  }
+}
